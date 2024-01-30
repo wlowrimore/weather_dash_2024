960f7c831d86b9f335cd5d8b68f4bd1e6b120464
William Lowrimore <wlowrimore@gmail.com>
2024-01-30 07:00:53 -0600

    adds loader

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='relative flex min-h-screen flex-col items-center justify-center p-24 md:mx-[16rem]'>
+      <div className='loader'></div>
+    </div>
+  )
+}
+
+export default Loading
