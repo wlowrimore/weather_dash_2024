91cf161c43c0fbf32727a08ed65e6613d61b9d55
William Lowrimore <wlowrimore@gmail.com>
2024-01-30 09:46:51 -0600

    favicon in layout metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "My Weather",
   description: "Weather app for Next.js.  Search for any city in the world to see the current weather and 5-day forecast.",
+  icons: {
+    icon: "/favicon.ico",
+  }
 };
 
 export default function RootLayout({ children }) {
