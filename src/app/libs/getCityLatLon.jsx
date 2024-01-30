7abfa36dda37ee557170687fc5d8377c18446578
William Lowrimore <wlowrimore@gmail.com>
2024-01-30 09:09:37 -0600

    fixes api url from http to https

--- src/app/libs/getCityLatLon.jsx
+++ src/app/libs/getCityLatLon.jsx
@@ -1,7 +1,7 @@
 export default async function getCityLatLong(city, state, countryCode) {
   const API_KEY = process.env.NEXT_WEATHER_API_KEY;
 
-  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${countryCode}&appid=${API_KEY}`);
+  const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${countryCode}&appid=${API_KEY}`);
 
   if (!response.ok) {
     throw new Error('Failed to fetch this city\'s latitude and longitude');
